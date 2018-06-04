b005b318ee902492ec14a3d87ec4cc718778876a
Knut Leborg <knut.leborg@telecomputing.no>
2018-06-04 14:20:25 +0200

    extend workorder create interface field map

--- src/API/WorkOrder.php
+++ src/API/WorkOrder.php
@@ -12,11 +12,15 @@ class WorkOrder extends RemedyCase
 
     protected static $createInterfaceFieldMap = [
         "Case_Number" => "WorkOrder_ID",
-		"Entry_ID" => "Request ID"
+        "Entry_ID" => "Request ID",
+        
+        "Assigned_Support_Company" => "Support Company",
+        "Assignee" => "Request Assignee",
+        "Description" => "Summary"
     ];
 
     protected static $standardInterfaceFieldMap = [
-		"Customer_Company" => "Assigned Support Company",
+		#"Customer_Company" => "Assigned Support Company", ??
 		"Support_Organization" => "Assigned Support Organization",
 
 		"Support_Group_ID" => "Assigned Group ID",
