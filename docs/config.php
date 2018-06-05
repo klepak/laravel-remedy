3ed6338b77d200470fcd71238e053f2a9d13f416
Knut Leborg <knut.leborg@telecomputing.no>
2018-06-05 14:11:22 +0200

    install and configure sami

---
+++ docs/config.php
@@ -0,0 +1,18 @@
+<?php
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('tests')
+    ->in('./src');
+
+return new Sami($iterator, array(
+    'title'                => 'laravel-remedy',
+    'build_dir'            => __DIR__.'/build',
+    'cache_dir'            => __DIR__.'/cache',
+    #'remote_repository'    => new GitHubRemoteRepository('username/repository', '/path/to/repository'),
+    'default_opened_level' => 2,
+));
